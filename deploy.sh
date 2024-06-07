bb7314bd409985094e3349503e43ddbc8437569c
Stefan Cruz <17972991+stefancruz@users.noreply.github.com>
2024-06-07 08:28:49 +0100

    chore: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,18 @@ touch $DOCUMENT_ROOT/maintenance.file
 
 # Swap over the content
 echo "Deploying content..."
-mkdir -p $DOCUMENT_ROOT/Odysee
-cp odysee.png $DOCUMENT_ROOT/Odysee
-cp OdyseeConfig.json $DOCUMENT_ROOT/Odysee
-cp OdyseeScript.js $DOCUMENT_ROOT/Odysee
-sh sign.sh $DOCUMENT_ROOT/Odysee/OdyseeScript.js $DOCUMENT_ROOT/Odysee/OdyseeConfig.json
+mkdir -p $DOCUMENT_ROOT/Dailymotion
+cp build/dailymotion.png $DOCUMENT_ROOT/Dailymotion
+cp build/DailymotionConfig.json $DOCUMENT_ROOT/Dailymotion
+cp build/DailymotionScript.js $DOCUMENT_ROOT/Dailymotion
+sh sign.sh $DOCUMENT_ROOT/Dailymotion/DailymotionScript.js $DOCUMENT_ROOT/Dailymotion/DailymotionConfig.json
 
 # Notify Cloudflare to wipe the CDN cache
 echo "Purging Cloudflare cache for zone $CLOUDFLARE_ZONE_ID..."
 curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
      -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
      -H "Content-Type: application/json" \
-     --data '{"files":["https://plugins.grayjay.app/Odysee/odysee.png", "https://plugins.grayjay.app/Odysee/OdyseeConfig.json", "https://plugins.grayjay.app/Odysee/OdyseeScript.js"]}'
+     --data '{"files":["https://plugins.grayjay.app/Dailymotion/Dailymotion.png", "https://plugins.grayjay.app/Dailymotion/DailymotionConfig.json", "https://plugins.grayjay.app/Dailymotion/DailymotionScript.js"]}'
 
 # Take site back online
 echo "Bringing site back online..."
