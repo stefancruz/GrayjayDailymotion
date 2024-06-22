f72f07fa5516be5abbb85c7b94bce54d15cea9e8
Stefan Cruz <17972991+stefancruz@users.noreply.github.com>
2024-06-22 15:22:16 +0100

    chore: configuration adjustments

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ touch $DOCUMENT_ROOT/maintenance.file
 # Swap over the content
 echo "Deploying content..."
 mkdir -p $DOCUMENT_ROOT/Dailymotion
-cp build/dailymotion.png $DOCUMENT_ROOT/Dailymotion
+cp build/DailymotionIcon.png $DOCUMENT_ROOT/Dailymotion
 cp build/DailymotionConfig.json $DOCUMENT_ROOT/Dailymotion
 cp build/DailymotionScript.js $DOCUMENT_ROOT/Dailymotion
 sh sign.sh $DOCUMENT_ROOT/Dailymotion/DailymotionScript.js $DOCUMENT_ROOT/Dailymotion/DailymotionConfig.json
@@ -18,7 +18,7 @@ echo "Purging Cloudflare cache for zone $CLOUDFLARE_ZONE_ID..."
 curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
      -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
      -H "Content-Type: application/json" \
-     --data '{"files":["https://plugins.grayjay.app/Dailymotion/Dailymotion.png", "https://plugins.grayjay.app/Dailymotion/DailymotionConfig.json", "https://plugins.grayjay.app/Dailymotion/DailymotionScript.js"]}'
+     --data '{"files":["https://plugins.grayjay.app/Dailymotion/DailymotionIcon.png", "https://plugins.grayjay.app/Dailymotion/DailymotionConfig.json", "https://plugins.grayjay.app/Dailymotion/DailymotionScript.js"]}'
 
 # Take site back online
 echo "Bringing site back online..."
